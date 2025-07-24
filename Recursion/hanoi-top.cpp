2125de75619eb8c6583d008e8253601ab2ea90f7
leedongkyu <leedongkyu0225@gmail.com>
2025-07-24 12:15:18 +0900

    fix: hanoitop test

--- Recursion/hanoi-top.cpp
+++ Recursion/hanoi-top.cpp
@@ -1,13 +1,20 @@
+#include <cmath>
 #include <iostream>
 void TOH(int n, int A, int B, int C) {
   if (n > 0) {
     TOH(n - 1, A, C, B);
-    printf("(%d,%d)\n", A, C);
+    printf("%d %d\n", A, C);
+
     TOH(n - 1, B, A, C);
   }
+  // printf("%d", num);
 }
 
 int main(int argc, char const *argv[]) {
-  TOH(3, 1, 2, 3);
+  int n = 0;
+  std::cin >> n;
+  int result = pow(2, n);
+  printf("%d\n", result - 1);
+  TOH(n, 1, 2, 3);
   return 0;
 }
