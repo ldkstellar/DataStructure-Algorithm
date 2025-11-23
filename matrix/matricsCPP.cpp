34942b55dfd248bb4ec2566dcfe04f3209feae06
leedongkyu <leedongkyu@0225@gmail.com>
2025-11-23 22:38:47 +0900

    feat: 뚱땡이 아저씨 코드 잣같게 올리네

---
+++ matrix/matricsCPP.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+class Diagonal {
+ public:
+  Diagonal() : n(2), A(new int[2]) {}
+
+  Diagonal(int n) : n(n), A(new int[n]) {}
+
+  ~Diagonal() { delete[] A; }
+
+  void Set(int i, int j, int x);
+
+  int Get(int i, int j);
+
+  void Display();
+
+ private:
+  int* A;
+  int n;
+};
+
+void Diagonal::Set(int i, int j, int x) {
+  if (i == j) A[i - 1] = x;
+}
+int Diagonal::Get(int i, int j) {
+  if (i == j) return A[i - 1];
+  return 0;
+}
+
+void Diagonal::Display() {
+  for (int i = 1; i <= n; i++) {
+    for (size_t j = 1; j <= n; j++) {
+      if (i == j)
+        std::cout << A[i - 1] << " ";
+      else
+        std::cout << "0 ";
+    }
+    std::cout << std::endl;
+  }
+}
+
+int main(int argc, char const* argv[]) {
+  Diagonal d(4);
+  d.Set(1, 1, 5);
+  d.Set(2, 2, 8);
+  d.Set(3, 3, 9);
+  d.Set(4, 4, 12);
+  d.Display();
+  return 0;
+}
