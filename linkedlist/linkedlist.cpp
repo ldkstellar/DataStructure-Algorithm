254114239cfef5f78ededb8afbc74630d0177ce0
leedongkyu <leedongkyu0225@gmail.com>
2025-12-08 11:26:12 +0900

    feat 재귀적 링크드리스트 출력 및 디버깅시 조사식쓰는법 읽을때는 원본데이터가 가르키는 방향이 바뀌지 않더록하기

--- linkedlist/linkedlist.cpp
+++ linkedlist/linkedlist.cpp
@@ -3,38 +3,55 @@ class Node {
  public:
   Node* next;  // 다음 노드
   int data;
-  void create(int[], int);
-  void display();
 };
 
-Node* first;
-
-void Node::create(int A[], int n) {  // 전역변수 first의 포인터가 숨어있다.
+Node* first;  // 전역시작 노드
+int sample = 1;
+// 링크드 리스트 생성
+void create(int A[],
+            int n) {  // 전역변수 first의 포인터가 숨어있다. 함수 파라미터에
   Node *t, *last;
   first = new Node;
   first->data = A[0];
-  first->next = NULL;
+  first->next = nullptr;
   last = first;
   for (size_t i = 1; i < n; i++) {
     t = new Node;
     t->data = A[i];
-    t->next = NULL;
+    t->next = nullptr;
     last->next = t;
     last = t;
   }
 }
 
-void Node::display() {  // 밑에도 이하동문
-  while (first != NULL) {
-    printf("%d ", first->data);
-    first = first->next;
+void display() {  // 밑에도 이하 동문
+  // 함수 내부 조사식에 영향을 미친다.//read값은 포인터나 값복사 사용 원본을
+  // 사용하면 원본 손상 이어날 가능성이 있다.
+  Node* p = first;
+  while (p != nullptr) {
+    printf("%d ", p->data);
+    p = p->next;
   }
 }
 
+void RDisplay(Node* p) {
+  if (p != nullptr) {
+    std::cout << p->data;
+    RDisplay(p->next);
+  }
+}
+
+void showSample() {
+  sample -= 2;
+  printf("%d", sample);
+}
 int main(int argc, char const* argv[]) {
   int A[] = {3, 5, 7, 10, 15};
-  Node linkedList;          // 공간만 잡아 놓음
-  linkedList.create(A, 5);  // 여가서 전역변수의 first를 사용하여 만든다,
-  linkedList.display();
+  ;  // 공간만 잡아 놓음
+  create(A, 5);
+
+  // 여가서 전역변수의 first를 사용하여 만든다,
+  display();
+  RDisplay(first);
   return 0;
 }
